aebd82e704f3d1a2ee95e59ebd975a62d30c50d8
jelliot <jecosgro@gmail.com>
2016-12-25 14:00:18 +0000

    Formatting and clarification

--- eev/RQL examples.R
+++ eev/RQL examples.R
@@ -1,24 +1,20 @@
 library("RQuantLib")
 
-## Example data taken from AKAM EEV example 
-Spot     <- 47.29
-Strike   <- 46
+# Static example data taken from EA EEV example 
+Spot     <- 72.05
+Strike   <- 81
 riskfr   <- 0.005
 dividend <- 0
-TTM      <- 2/252
-Cprice   <- 3.05
+TTM      <- 6/252
+Cprice   <- 0.355
 
 rq_iv = EuropeanOptionImpliedVolatility("call", Cprice, Spot, Strike, dividend,
                                         riskfr, TTM, 0.1)
-cat("Excel thinks Calculated IV is 141.447%, but RQL thinks it's: ", 
+cat("Excel thinks Calculated IV is 60.32%, but RQL thinks it's: ", 
     rq_iv[1]*100, "%")
+##############################
 
-strikes = seq(42, 48.5, by=0.5)
-vols = seq(0.01, 0.14, by=0.01)
-
-iv_array = EuropeanOptionImpliedVolatility
-
-# these will prob change every time
+# example on whole array to see why excel != RQL
 analysis_date   = "2015-08-27"
 #analysis_datep1 = analysis_date + 1 # broken
 first_u_date    = "2015-08-28"
@@ -37,24 +33,25 @@ myivprice    = myivprice[!is.na(myivprice$IV30),] # no NA iv
 underly      = mydata[1]$Last
 mydata       = mydata[-1,]                        # top row is underlying
 
-# Set up only to use those strikes with bids/asks?
+# Set up only to use those strikes with bids/asks, look for Date
 matrixComplete = mydata[!is.na(mydata$Date),]
 
 # What other filters apply for the IEV calculation?
 
 # Do all the date math
-mydata$isoDate = as.Date(mydata$Date, "%y%m%d") # will error if any NAs
-mydata$ND      = 1                              # total days
-mydata$ED      = 1                              # earnings days
-mydata$BD      = 1                              # non-earnings days
+matrixComplete$isoDate = as.Date(mydata$Date, "%y%m%d") # will error if any NAs
+matrixComplete$ND      = 1                              # total days
+matrixComplete$ED      = 1                              # earnings days
+matrixComplete$BD      = 1                              # non-earnings days
 
-for (i in 1:length(mydata$Strike.Price)) {
-  mydata[i]$CalcIV = AmericanOptionImpliedVolatility("call", 
-                                                     mydata[i]$Last,
-                                                     underly,
-                                                     mydata[i]$Strike.Price,
-                                                     dividend, 
-                                                     riskfr, 
-                                                     mydata[i]$ND,
-                                                     0.1)
+for (i in 1:length(matrixComplete$Strike.Price)) {
+  matrixComplete[i]$CalcIV = 
+    AmericanOptionImpliedVolatility("call", 
+                                    matrixComplete[i]$Last,
+                                    underly,
+                                    matrixComplete[i]$Strike.Price,
+                                    dividend, 
+                                    riskfr, 
+                                    matrixComplete[i]$ND,
+                                    0.1)
 }

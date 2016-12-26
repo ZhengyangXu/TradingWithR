eaa94d5fe6be5ddde459dfb913903162a6324161
jelliot <jecosgro@gmail.com>
2016-12-26 22:07:29 +0000

    Change sys to Sys for R warnings sake

--- eev/httr_demo.R
+++ eev/httr_demo.R
@@ -3,7 +3,7 @@ library(xml2)
 library(XML)
 library(dplyr)
 r = GET("https://www.optionslam.com/accounts/login/")
-sys.sleep(1)
+Sys.sleep(1)
 cookies(r)
 token=cookies(r)[1,]$value
 mypass = .rs.askForPassword("Optionslam password:")
@@ -16,12 +16,12 @@ p = POST("https://www.optionslam.com/accounts/login/",
          add_headers('Referer' = "https://www.optionslam.com/accounts/login/",
                      'Origin'  = "https://www.optionslam.com"))
 rm(mypass)
-sys.sleep(1)
+Sys.sleep(1)
 #debug
 #http_status(p)
 #headers(p)
 r2 = GET("https://www.optionslam.com/earnings/stocks/SBUX?page=-1")
-sys.sleep(1)
+Sys.sleep(1)
 #http_status(r2)
 t = readHTMLTable(content(r2, "text"))
 
@@ -38,3 +38,4 @@ colnames(past_earnings) = table_headings
 
 # Grab the last 12 dates for later...
 foo = as.numeric(sub("%", "", past_earnings$`CLOSE%`[1:12]))/100
+

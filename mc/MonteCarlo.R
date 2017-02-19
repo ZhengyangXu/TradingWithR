7b518061ed24b2abe326fed248670161400d4725
jelliot <jecosgro@gmail.com>
2017-02-18 22:54:37 -0500

    A quickly written, slow performing equity curve simulator

---
+++ mc/MonteCarlo.R
@@ -0,0 +1,85 @@
+# Try to simulate some equity curves given P(win) and Avg Win size
+# Inputs:
+#   number of trades per curve
+#   number of curves
+#   probability of winning
+#   profit factor -OR- (average win size + average loss size) -OR- W:L
+#   whether wins/losses are in $ or %
+#   dollar multiplier of W:L if W:L in $ 
+#     (e.g. each trade wins $50 and loses $100 if W:L is 0.5 and mult is 100)
+#   percent multiplier of W:L if W:L in % 
+#     (e.g. each trade wins 5% and loses 10% if W:L is 0.5 and mult is 10)
+#   starting equity
+# Risk of ruin inputs:
+#   Loss level % (be clear what this is: losing X% or having X% remaining)
+# Outputs:
+#   Basic statistical analysis of all curve results
+#   Basic statistical analysis of drawdowns by time
+#     answer the question: "how hard is this to stick to when times are bad?"
+#   Basic statistical analysis of drawdowns by amount
+#     answer the question: "how pissed would people be looking at results?"
+#   Graph of random sample of X curves
+# Risk of ruin outputs:
+#   Risk of ever experiencing loss level % from any given point on curve
+#   Risk of ever experiencing loss level % of starting capital
+
+library(zoo)
+
+pwin   = 0.9
+wlr    = 0.4
+#avg_w  = 2
+#avg_l  = 5
+if (wlr < 1) {
+  avg_w = 1
+  avg_l = (1/wlr)
+} else {
+  avg_w = wlr
+  avg_l = 1
+}
+#wlr    = avg_w / avg_l
+#profac = wlr * (pwin/(1-pwin))
+d_mult = 100
+f_risk = 10
+ncurve = 100
+ntrade = 100
+d_or_f = 'd'
+equity = 10000
+
+probs  = runif(ncurve * ntrade)
+probs  = matrix(probs, nrow=ntrade, ncol=ncurve)
+
+trades = probs
+if (d_or_f == "d") {
+  trades[probs <= pwin] = avg_w * d_mult
+  trades[probs > pwin]  = avg_l * d_mult * (-1)
+} else {
+  trades[probs <= pwin] = avg_w * f_risk # this is broken
+  trades[probs > pwin]  = avg_l * f_risk * (-1) # this is broken
+}
+
+curves = rep(equity, ncurve)
+temp   = rbind(curves, trades)
+#curves = rollapply(curves, 1, sum) # not this easy
+
+excel_roll_sum = function(x) {
+  y = x
+  for (i in 1:length(x)) { y[i] = rollapply(x, i, sum)[1]}
+  return(y)
+}
+
+curves  = apply(temp, 2, excel_roll_sum)
+avg_crv = rowSums(curves)/ncurve
+
+# plot them
+xrange = c(0, ntrade)
+yrange = range(curves)
+
+plot(xrange, yrange, type="n", xlab="# trades", ylab="$ equity")
+colors   = rainbow(ncurve)
+linetype = c(1:ncurve)
+
+for (i in 1:ncurve) {
+  lines(1:(ntrade+1), curves[,i], col=colors[i])
+}
+
+lines(1:(ntrade+1), avg_crv, col="black", lwd=3)

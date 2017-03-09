3ef911285f69e21f6d5844d07a7ecab6bf2e6f74
jelliot <jecosgro@gmail.com>
2017-03-08 22:49:31 -0500

    Compiles and runs, but performance doesn't match author

--- rotation/ETFRotation.R
+++ rotation/ETFRotation.R
@@ -41,16 +41,25 @@ library("RcppRoll") # no xts/zoo support?
 #            "DBC", "GLD", # DBC starts 2006-02-06, GLD 2004-11-18
 #            "ICF", "RWX") # RWX starts 2006-12-19
 
-symbols = c("SPY", "EFA", "IEF", "GLD", "ICF", "DBC")
-adjust_for_dividends = 6 # 6 is adjusted close, 4 is close
-initBal = 30000
-nFunds  = 3
+symbols = c("SPY", "EFA", "IEF", "GLD", "ICF")
+adjust_for_dividends = 4 # 6 is adjusted close, 4 is close
+initBal   = 60000
+nFunds    = 3
+data.start.year = 2007
+data.start.mon  = 1
+data.end.year   = 2017
+data.end.mon    = 1
+data.start.time = paste(data.start.year, data.start.mon, 1, sep='-')
+data.end.time   = paste(data.end.year, data.end.mon, 1, sep='-')
+
+analysis.start.year  = 2007
+analysis.start.mon   = 12
 
 # Get some dataz
 for (sym in symbols) {
   #getSymbols(sym, from="2002-01-01", to="2014-12-31") # need this data
   #getSymbols(sym, from="2007-02-01", to="2016-12-31")
-  #getSymbols(sym, from="2015-01-01", to="2017-03-01")
+  getSymbols(sym, from=data.start.time, to=data.end.time)
   if (sym == symbols[1]) {
     adjCl = get(sym)[,adjust_for_dividends]
   } else {
@@ -106,23 +115,27 @@ getRanks = function(foo, A, B, C) {
 # output: bottom 3 ranking (that is, best) performing symbols to choose
 pickThree = function(foo) {
   bar = sort(foo)[1:3]
-  baz = c(symbols[foo == bar[1]], 
-          symbols[foo == bar[2]], 
-          symbols[foo == bar[3]])
-  return (baz[1:3]) # forces first 3 entries if ties, e.g. (1st, 1st, 2nd)
-                    # ties don't matter; you want 1 1 2 if possible
-                    # if you chop 1 2 3 (3) no big deal
+  if (length(symbols[foo == bar[1]]) == 2)
+    baz = c(symbols[foo == bar[1]], 
+            symbols[foo == bar[3]])
+  else if (length(symbols[foo == bar[1]]) == 3)
+    baz = symbols[foo == bar[1]]
+  else
+    baz = c(symbols[foo == bar[1]], 
+            symbols[foo == bar[2]], 
+            symbols[foo == bar[3]])
+  return (baz[1:3])
 }
 
 # example output for the start of December 2016
-pickThree(getRanks(last(adjCl["2016-7"], '1 day'), 0.3, 0.4, 0.3))
+#pickThree(getRanks(last(adjCl["2016-7"], '1 day'), 0.3, 0.4, 0.3))
 
 
 
 
 
 # matrix of daily returns
-returns     = apply(adjCl[,1:6], 2, Delt, type='arithmetic')
+returns     = apply(adjCl[,1:length(symbols)], 2, Delt, type='arithmetic')
 returns     = xts(returns, order.by=index(adjCl))
 returns[1,] = 0 # cause Delt() made row 1 == NA
 
@@ -136,54 +149,119 @@ balances = xts(
            )
 colnames(balances) = symbols
 
-# get first recommendations (y will contain what to invest in for 2016-01)
-y = pickThree(getRanks(last(adjCl["2015-12"], '1 day'), 0.3, 0.4, 0.3))
-# set initial balances for funds (only 3 future funds will use this)
-balances[index(last(balances["2015-12"], '1 day')),] = (initBal/nFunds)
-# if you are switching funds and you have only 1 fund you're swapping
-# your new fund's balance is you old fund's balance
-# else if you are switching funds and you have 2 funds you're swapping
-# your new two funds balances are (old fund 1 + old fund 2) / 2
-#        how to handle this and still be able to rowSums()?
-#        and what about the return on those days? 
-#              find which are the new funds and switch balances?
+### try to do everything in for loop
+#### get first recommendations (y will contain what to invest in for 2016-01)
+###y = pickThree(getRanks(last(adjCl["2015-12"], '1 day'), 0.3, 0.4, 0.3))
+#### set initial balances for funds (only 3 future funds will use this)
+###balances[index(last(balances["2015-12"], '1 day')),] = (initBal/nFunds)
+#### first, put returns into balances, then do cumprod()
+###for (i in 1:length(y)) {
+###  balances["2016-1",which(symbols == y[i])] = 1+returns["2016-1",which(symbols == y[i])]
+###}
+#### get the actual balances for the month
+###balances["2016-1",] = cumprod(rbind(last(balances["2015-12"], '1 day'), balances["2016-1"]))["2016-1",]
 
-# first, put returns into balances, then do cumprod()
-for (i in 1:length(y)) {
-  balances["2016-1",which(symbols == y[i])] = 1+returns["2016-1",which(symbols == y[i])]
-}
-# get the actual balances for the month
-balances["2016-1",] = cumprod(rbind(last(balances["2015-12"], '1 day'), balances["2016-1"]))["2016-1",]
 # now you have the returns for 2016-1, and you repeat the loop since you 
 # can reference the previous returns. but now that you're repeating the loop
 # you have to do error checking / see if you're already invested in something
 
-x = pickThree(getRanks(last(adjCl["2016-1"], '1 day'), 0.3, 0.4, 0.3))
-a = x[(x %in% y) == TRUE]  # funds in y (and x) that will stay invested
-b = y[(y %in% x) == FALSE] # funds in y that will swap out
-c = x[(x %in% y) == FALSE] # funds in x that will be new
-# if a == 3, do nothing
-# else if a == 2 (i.e. one fund swaps) 
-#   new fund last day performance == last day old fund performance
-#   old fund last day performance = 0
-# else if a == 1 (i.e. two funds swap)
-#   temp sum = last day old fund 1 + last day old fund 2
-#   new fund 1 = temp / 2
-#   new fund 2 = temp / 2
-#   old funds last day performance = 0
+###x = pickThree(getRanks(last(adjCl["2016-1"], '1 day'), 0.3, 0.4, 0.3))
+###a = x[(x %in% y) == TRUE]  # funds in y (and x) that will stay invested
+###b = y[(y %in% x) == FALSE] # funds in y that will swap out
+###c = x[(x %in% y) == FALSE] # funds in x that will be new
 
-
-
-
-
-
-
-for (y in 2016:2017) {
+# build the list of picks
+# l$"2016-1" is what to invest in at the end of the month for 2016-2
+l = list()
+tmp = paste(analysis.start.year, 12, sep='-')
+l[[tmp]] = pickThree(getRanks(last(adjCl[paste(tmp)], '1 day'), 0.3, 0.4, 0.3))
+for (y in (analysis.start.year+1):(data.end.year-1)) {
   for (m in 1:12) {
     if (y == 2017 && m > 2) {
       break
     } else {
-      print(c(paste(y, m, sep='-'), pickThree(getRanks(last(adjCl[paste(y, m, sep='-')], '1 day'), 0.3, 0.4, 0.3))))
+        tmp = paste(y, m, sep='-')
+        l[[tmp]] = pickThree(getRanks(last(adjCl[tmp], '1 day'), 0.3, 0.4, 0.3))
     }
   }
 }
+old = names(l)[1]
+new = names(l)[2]
+for (sym in l[[old]]) {
+  balances[index(last(balances[paste(old)], '1 day')),which(symbols == sym)] = (initBal/nFunds)
+}
+
+for (i in 2:length(l)) {
+  # calculate balances into next month
+  # calculate balances for your new month, then get new month names
+  # DON'T CONFUSE new AND l[[new]] !!!!
+  for (sym in l[[old]]) 
+    balances[paste(new),which(symbols == sym)] = 
+      1+returns[paste(new),which(symbols == sym)]
+  
+  balances[paste(new),] = cumprod(rbind(last(balances[paste(old)], '1 day'), balances[paste(new)]))[paste(new),]
+
+  a = l[[old]][(l[[old]] %in% l[[new]]) == TRUE]  # funds in old (and new) that will stay invested
+  b = l[[old]][(l[[old]] %in% l[[new]]) == FALSE] # funds in old that will swap out
+  c = l[[new]][(l[[new]] %in% l[[old]]) == FALSE] # funds in new that will be new
+  ############### should all the 'news' below be 'olds' if prework is done?
+  ############### and not include 2015 in list?
+  # crux is that you made your decisions for january returns on 12/31
+  #   then you filled out returns from january
+  #   then calculated what 'old' and 'new' are -- REMEMBER l[["2016-1"]]
+  #   is what to invest in for 2016-2
+  if (length(a) == 3) {
+    # do nothing. old balances are new balances.
+  } else if (length(a) == 2) {
+    # only one fund to swap
+    # on last day of the current (?) month
+    # set new fund (c) balance to (old fund (b) * last return day of (b))
+    balances[paste(new),][nrow(balances[paste(new)]),which(symbols == c)] =
+      balances[paste(new),][nrow(balances[paste(new)]),which(symbols == b)]
+    # set old fund balance (b) to 0
+    balances[paste(new),][nrow(balances[paste(new)]),which(symbols == b)] = 0
+  } else if ((length(a) == 1) || (length(a) == 0)) {
+    # two funds to swap
+    # for each old fund, get previous balance, add to sum, set to 0
+    tempSum = 0
+    for (sym in b) {
+      tempSum = tempSum + as.numeric(
+                            last(balances[paste(new),which(symbols == sym)], 
+                                 '1 day'))
+      balances[paste(new),][nrow(balances[paste(new)]),which(symbols == sym)] = 0
+    }
+    # for each new fund, set new balance
+    for (sym in c) {
+      balances[paste(new),][nrow(balances[paste(new)]),which(symbols == sym)] =
+        tempSum / length(c)
+    }
+  } else {
+    print('broken')
+    break
+  }
+  old = names(l)[i] # pick better names here. invested?
+  if (i == length(l)) {
+    break
+  } else {
+    new = names(l)[i+1]   # toInvest?
+  }
+}
+
+equity = cbind(balances,rowSums(balances))[,length(symbols)+1][paste(analysis.start.year+1, 2017, sep="::"),]
+bench  = cumprod(
+          rbind(initBal * first(1+returns$SPY[paste(analysis.start.year+1),], '1 day'), 
+                first(1+returns$SPY[paste(analysis.start.year+1, 2017, sep="::"),], '-1 day')))
+##plot(last(bench, '-1 day'))
+##lines(last(equity["2017"], '-1 day'), col='red')
+plot(last(equity, '-1 day'))
+lines(last(bench, '-1 day'), col='red')
+
+# redo everything with log returns so you can use PerformanceSummary
+# graphs in quantmod?
+print(Delt(to.yearly(equity)[,4]))
+# something very wrong with backtest code
+# returns don't match LT's at all some years
+# TODO: confirm a test on 1 year of returns by hand in excel
+
+
+

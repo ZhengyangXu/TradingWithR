463368cbcdb92d689535085575f93b1cba9e893f
jelliot <jecosgro@gmail.com>
2017-05-27 20:41:01 -0400

    Add clarification to help myself later

--- eev/LP example.R
+++ eev/LP example.R
@@ -5,9 +5,10 @@
 #     StockB gives 4% return on average
 #     StockC gives 6% return on average
 
-# To set some constraints, lets say my investment in C must be less than sum of
-#   A, B.  Also, investment in A cannot be more than twice of B.  Finally, at 
-#   least 10% of investment in each stock.
+# To set some constraints, lets say 
+#   my investment in C must be less than sum of A, B.  
+#   Also, investment in A cannot be more than twice of B.  
+#   Finally, at least 10% of investment in each stock.
 
 # To formulate this problem:
   
@@ -26,13 +27,13 @@ library(lpSolveAPI)
 
 # set the number of variables
 model = make.lp(0, 3)
-# define the objective function: for minimize, use -ve (wtf?)
-set.objfn(model, c(-0.05, -0.04, -0.06))
+# define the objective function: for minimize, use negative
+set.objfn(model, c(-0.05, -0.04, -0.06)) # = x1*5% + x2*4% + x3*6%
 # add constraints
-add.constraint(model, c(1,  1,  1), "=", 1)
-add.constraint(model, c(1,  1, -1), ">", 0)
-add.constraint(model, c(1, -2,  0), "<", 0)
-# set upper and lower bounds
+add.constraint(model, c(1,  1,  1), "=", 1) #  x1 + x2 + x3 = 1
+add.constraint(model, c(1,  1, -1), ">", 0) #  x1 + x2 - x3 > 0
+add.constraint(model, c(1, -2,  0), "<", 0) #  x1 - 2*x2    < 0
+# set upper and lower bounds #  10% < x1, x2, x3 < 100%
 set.bounds(model, lower=c(0.1, 0.1, 0.1), upper=c(1, 1, 1))
 # compute model
 solve(model)
@@ -44,4 +45,4 @@ print(get.variables(model))
 cat("solved obj function result:")
 print(get.objective(model))
 # get constraints, that is, what did we constrain the systems of equations to?
-#get.constraints(model)
+#get.constraints(model) # 1 0 0 see above, it's the last arg to add.constraint
